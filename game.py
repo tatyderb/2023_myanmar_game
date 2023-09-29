def6d3430b9d4b29a885668f34b7275dd5ce3089
Your Name <tatyderb@gmail.com>
2023-09-29 17:35:59 +0300

    draw player

--- game.py
+++ game.py
@@ -17,7 +17,9 @@ display.blit(background_img, (0, 0))
 # шрифт для букв
 sysfont = pg.font.SysFont('arial', 40)
 text_img = sysfont.render('Score: 123', True, 'red')
+# display.blit(text_img, (100, 200))
 
+'''
 font = pg.font.Font('resources/font/04B_19__.TTF', 48)
 game_over_img = font.render('Game Over', True, 'white')
 w = game_over_img.get_width()
@@ -25,12 +27,20 @@ h = game_over_img.get_height()
 x = screen_width/2 - w/2
 y = screen_height/2 - h/2
 display.blit(game_over_img, (x, y))
+'''
+
+# player
+player_img = pg.image.load('resources/img/player.png')
+player_width = player_img.get_width()
+player_height = player_img.get_height()
+player_x = screen_width / 2 - player_width / 2
+player_y = screen_height - player_height
 
 running = True
 while running:
     # рисовать
-    display.fill('blue', (20, 50, 100, 250))
-    display.blit(text_img, (100, 200))
+    # display.fill('blue', (20, 50, 100, 250))
+    display.blit(player_img, (player_x, player_y))
     pg.display.update()
 
     # получить события игры и обработать их
@@ -39,7 +49,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
         # нажали на клавишу q и закрыли окно
-        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+        if event.type == pg.KEYDOWN and event.key == pg.K_q:
             running = False
 
 
